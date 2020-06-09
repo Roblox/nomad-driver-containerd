f947b0272482f2fbf1351c6aceb283687ced1cc4
Shishir Mahajan <smahajan@roblox.com>
2020-06-09 08:44:48 -0700

    Fix review comments.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -16,25 +16,18 @@ main() {
 	echo "Starting setup."
 	setup
 	echo "Setup finished successfully."
+
 	echo "Checking if nomad-driver-containerd is up and running, and nomad is ready to accept jobs."
-	while true
-	do
-		set +e
-		status=$(curl -s http://127.0.0.1:4646/v1/nodes|jq '.[0] ."Drivers" ."containerd-driver" ."Healthy"')
-		rc=$?
-		set -e
-		if [[ $rc -eq 0 && $status = "true" ]]; then
-			echo "nomad is up and running"
-			break
-		fi
-		echo "nomad is down, wait for 5 seconds."
-		sleep 5s
-	done
+	is_containerd_driver_active
+
 	cd ~/go/src/github.com/Roblox/nomad-driver-containerd/example
+
 	echo "Starting nomad redis job using nomad-driver-containerd."
 	nomad job run redis.nomad
+
 	echo "Starting nomad signal handler job using nomad-driver-containerd."
 	nomad job run signal.nomad
+
 	echo "Checking status of redis job."
 	redis_status=$(nomad job status -short redis|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
 	if [ $redis_status != "running" ];then
@@ -45,40 +38,36 @@ main() {
 	# The actual container process might not be running yet.
 	# We need to wait for actual container to start running before trying exec.
 	echo "Wait for redis container to get into RUNNING state, before trying exec."
-	set +e
-	while true
-	do
-		sudo CONTAINERD_NAMESPACE=nomad ctr task ls|grep -q RUNNING
-		if [ $? -eq 0 ]; then
-			echo "redis container is up and running"
-			break
-		fi
-	done
-	set -e
+	is_redis_container_active
+
 	echo "Checking status of signal handler job."
 	signal_status=$(nomad job status -short signal|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
         if [ $signal_status != "running" ];then
 		echo "Error in getting signal handler job status."
 		exit 1
 	fi
+
 	echo "Inspecting redis job."
 	redis_status=$(nomad job inspect redis|jq -r '.Job .Status')
 	if [ $redis_status != "running" ];then
 		echo "Error in inspecting redis job."
 		exit 1
 	fi
+
 	echo "Inspecting signal handler job."
 	signal_status=$(nomad job inspect signal|jq -r '.Job .Status')
 	if [ $signal_status != "running" ]; then
 		echo "Error in inspecting signal handler job."
 		exit 1
 	fi
+
 	echo "Exec redis job."
 	exec_output=$(nomad alloc exec -job redis echo hello_exec)
 	if [ $exec_output != "hello_exec" ]; then
 		echo "Error in exec'ing redis job."
 		exit 1
 	fi
+
 	echo "Stopping nomad redis job."
 	nomad job stop redis
 	redis_status=$(nomad job status -short redis|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
@@ -86,6 +75,7 @@ main() {
 		echo "Error in stopping redis job."
 		exit 1
 	fi
+
 	echo "Stopping nomad signal handler job."
 	nomad job stop signal
 	signal_status=$(nomad job status -short signal|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
@@ -111,7 +101,7 @@ setup() {
 	sudo apt-get install -y apt-utils curl runc unzip make build-essential
 
 	# Change $(pwd) to /tmp
-        cd /tmp
+	cd /tmp
 
 	# Install containerd 1.3.4
 	curl -L -o containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
@@ -119,7 +109,7 @@ setup() {
 	rm -f containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
 
 	# Drop containerd systemd unit file into /lib/systemd/system.
-        cat << EOF > containerd.service
+	cat << EOF > containerd.service
 # /lib/systemd/system/nomad.service
 [Unit]
 Description=containerd container runtime
@@ -146,13 +136,7 @@ EOF
         sudo systemctl daemon-reload
         echo "Starting containerd daemon."
         sudo systemctl start containerd
-        while true
-        do
-                if (systemctl -q is-active "containerd.service"); then
-                        echo "systemd containerd.service is up and running"
-                        break
-                fi
-        done
+	is_systemd_service_active "containerd.service"
 
 	# Remove default golang (1.7.3) and install a custom version (1.14.3) of golang.
 	# This is required for supporting go mod, and to be able to compile nomad-driver-containerd.
@@ -201,13 +185,68 @@ EOF
 	sudo systemctl daemon-reload
 	echo "Starting nomad server and nomad-driver-containerd."
 	sudo systemctl start nomad
-	while true
+	is_systemd_service_active "nomad.service"
+}
+
+is_containerd_driver_active() {
+	i="0"
+	while test $i -lt 5
 	do
-		if (systemctl -q is-active "nomad.service"); then
-			echo "systemd nomad.service is up and running"
+		set +e
+		status=$(curl -s http://127.0.0.1:4646/v1/nodes|jq '.[0] ."Drivers" ."containerd-driver" ."Healthy"')
+		rc=$?
+		set -e
+		if [[ $rc -eq 0 && $status = "true" ]]; then
+			echo "containerd driver is up and running."
 			break
 		fi
+		echo "containerd driver is down, sleep for 3 seconds."
+		sleep 3s
+		i=$[$i+1]
 	done
+
+	if [ $i -ge 5 ]; then
+		echo "containerd driver didn't come up. exit 1."
+		exit 1
+	fi
+}
+
+is_redis_container_active() {
+	set +e
+	i="0"
+	while test $i -lt 5
+	do
+		sudo CONTAINERD_NAMESPACE=nomad ctr task ls|grep -q RUNNING
+		if [ $? -eq 0 ]; then
+			echo "redis container is up and running"
+			break
+		fi
+		echo "redis container is down, sleep for 3 seconds."
+		sleep 3s
+		i=$[$i+1]
+	done
+	set -e
+
+	if [ $i -ge 5 ]; then
+		echo "redis container didn't come up. exit 1."
+		exit 1
+	fi
+}
+
+is_systemd_service_active() {
+	local service_name=$1
+	i="0"
+	while test $i -lt 5 && !(systemctl -q is-active "$service_name"); do
+		printf "%s is down, sleep for 3 seconds.\n" $service_name
+		sleep 3s
+		i=$[$i+1]
+	done
+
+	if [ $i -ge 5 ]; then
+		printf "%s didn't come up. exit 1.\n" $service_name
+		exit 1
+	fi
+	printf "%s is up and running\n" $service_name
 }
 
 main "$@"
