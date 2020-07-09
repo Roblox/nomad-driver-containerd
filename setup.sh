c7d64bc732c5bed75efae7a3828a6a431991a580
Shishir Mahajan <smahajan@roblox.com>
2020-07-09 12:06:33 -0700

    Add setup.sh.

---
+++ setup.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export CONTAINERD_VERSION=1.3.4
+
+main() {
+  echo "INFO: Welcome! nomad-driver-containerd setup."
+  check_root
+  check_os
+  read -p "INFO: Download containerd (Y/N)? Press Y to continue. " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+     echo "Aborting setup..."
+     exit 0
+  fi
+  pushd /tmp
+  curl -L --silent -o containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
+  tar -C /usr/local -xzf containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
+  rm -f containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
+  read -p "INFO: Drop systemd unit containerd.service into /lib/systemd/system/containerd.service (Y/N)? Press Y to continue. " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+     echo "Aborting setup..."
+     exit 0
+  fi
+  drop_containerd_unit_file
+  echo "INFO: Reload containerd.service systemd unit."
+  systemctl daemon-reload
+  echo "INFO: Starting containerd daemon."
+  systemctl start containerd
+  popd
+  read -p "INFO: Setup nomad server + nomad-driver-containerd (Y/N)? Press Y to continue. " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+     echo "Aborting setup..."
+     exit 0
+  fi
+  echo "INFO: Cleanup any old binaries."
+  make clean
+  echo "INFO: Build nomad-driver-containerd binary: containerd-driver."
+  make build
+  echo "INFO: Create plugin-dir for containerd-driver: /tmp/nomad-driver-containerd."
+  mkdir -p /tmp/nomad-driver-containerd
+  echo "INFO: Move containerd-driver to /tmp/nomad-driver-containerd."
+  mv containerd-driver /tmp/nomad-driver-containerd
+  drop_nomad_unit_file echo $PWD
+  echo "INFO: Reload nomad.service systemd unit."
+  systemctl daemon-reload
+  echo "INFO: Starting nomad server + nomad-driver-containerd."
+  systemctl start nomad
+  echo "INFO: Setup finished successfully."
+}
+
+drop_nomad_unit_file() {
+  local curr_dir=$1
+  # Drop nomad server (dev) + nomad-driver-containerd systemd unit file into /lib/systemd/system.
+  cat << EOF > nomad.service
+# /lib/systemd/system/nomad.service
+[Unit]
+Description=nomad server (dev) + nomad-driver-containerd
+Documentation=https://nomadproject.io
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/nomad agent -dev -config=$curr_dir/example/agent.hcl -plugin-dir=/tmp/nomad-driver-containerd
+KillMode=process
+Delegate=yes
+LimitNOFILE=1048576
+LimitNPROC=infinity
+LimitCORE=infinity
+TasksMax=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+mv nomad.service /lib/systemd/system/nomad.service
+}
+
+
+drop_containerd_unit_file() {
+  # Drop containerd systemd unit file into /lib/systemd/system.
+  cat << EOF > containerd.service
+# /lib/systemd/system/containerd.service
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target
+
+[Service]
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/local/bin/containerd
+KillMode=process
+Delegate=yes
+LimitNOFILE=1048576
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNPROC=infinity
+LimitCORE=infinity
+TasksMax=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+mv containerd.service /lib/systemd/system/containerd.service
+}
+
+
+check_root() {
+  if [ $(id -u) != 0 ]; then
+    echo "ERROR: Run as root user."
+    exit 1
+  fi
+}
+
+check_os() {
+  cat /etc/os-release|grep -q -i "Ubuntu"
+  if [ $? -ne 0 ];then
+     echo "ERROR: Unsupported host OS. Run tests on Ubuntu."
+     exit 1
+  fi
+}
+
+main "$@"
